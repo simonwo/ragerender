3ac831a588b07781fe3b620ea893f3c07fcbd5be
Simon Worthington <simon@simonwo.net>
2025-05-14 12:04:52 +1000

    Add Liquid renderer
    Liquid is a bit more complicated than ERB because it doesn't support
    quite as much complexity as ERB. So we have to manually handle some of
    the conditional cases and also convert function calls into filters, and
    also manually keep track of which end tags to print as Liquid requires
    specific end tags depending on opening tag.

---
+++ lib/ragerender/to_liquid.rb
@@ -0,0 +1,87 @@
+require_relative 'language'
+
+module RageRender
+  LIQUID_FUNCTIONS = {
+    'add' => 'plus',
+    'subtract' => 'minus',
+    'multiply' => 'times',
+    'divide' => 'divided_by',
+    'js' => 'escape', # TODO: check these do the same thing!
+  }
+
+  def self.render_value value
+    case value
+    when String
+      value =~ /^[0-9]+$/ ? value : "\"#{value}\""
+    when Language::Variable
+      value.path.join('.')
+    when nil
+      ""
+    end
+  end
+
+  def self.to_liquid document
+    tag_stack = Array.new
+
+    document.map do |chunk|
+      case chunk
+      when String
+        chunk
+
+      when Language::Variable
+        "{{ #{chunk.path.join('.')} }}"
+
+      when Language::Conditional
+        tag_stack << :endif
+
+        lhs = render_value chunk.lhs
+        rhs = render_value chunk.rhs
+        operator = chunk.operator
+        output = Array.new
+        case operator
+        when '~', '!~'
+          # case insensitive comparison: we need to manually do this
+          output << "{% assign lhs = #{lhs} | downcase %}"
+          output << "{% assign rhs = #{rhs} | downcase %}"
+          lhs = "lhs"
+          rhs = "rhs"
+          operator = {'~' => '==', '!~' => '!='}[operator]
+        when '%', '!%'
+          # modulo comparison with zero
+          output << "{% assign lhs = #{lhs} | modulo: #{rhs} %}"
+          lhs = "lhs"
+          rhs = "0"
+          operator = {'%' => '==', '!%' => '!='}[operator]
+        end
+
+        operator = (operator == '=' ? '==' : operator)
+        output << "{% #{chunk.reversed ? 'unless' : 'if'} #{lhs} #{operator} #{rhs} %}"
+        output.join
+
+      when Language::Function
+        params = chunk.params.map {|p| render_value p }
+        name = LIQUID_FUNCTIONS.fetch(chunk.name, chunk.name)
+        args = params.drop(1).map {|p| "#{name}: #{p}" }.join(' | ')
+        "{{ #{params.first} | #{args} }}"
+
+      when Language::Loop
+        tag_stack << :endfor
+        "{% for l in #{chunk.path.join('.')} %}"
+
+      when Language::Layout
+        "{{ #{chunk.name} }}"
+
+      when Language::EndTag
+        if tag_stack.empty?
+          '[/]'
+        else
+          "{% #{tag_stack.pop.to_s} %}"
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  puts RageRender.to_liquid(RageRender::Language::parse(ARGF)).join
+end
