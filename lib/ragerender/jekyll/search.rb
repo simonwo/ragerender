0f1f8a7bbd758df7abe14f2ee0141533e96adb3c
Simon Worthington <simon@simonwo.net>
2026-05-02 12:13:20 +1000

    Paginate search when over 60 results
    We don't provide any access to the `[l:pages]` variable yet, so we
    can'y navigate between them.

--- lib/ragerender/jekyll/search.rb
+++ lib/ragerender/jekyll/search.rb
@@ -1,3 +1,7 @@
+require 'jekyll/generator'
+require 'jekyll/drops/drop'
+require_relative 'pagination'
+
 Jekyll::Hooks.register :pages, :pre_render do |page, payload|
   if page.data['layout'] == 'search'
     RageRender::Pipettes.clean_payload payload
@@ -6,11 +10,60 @@ Jekyll::Hooks.register :pages, :pre_render do |page, payload|
 end
 
 module RageRender
+  RESULTS_PER_PAGE = 60
+
+  class SearchPaginationGenerator < Jekyll::Generator
+    priority :low
+
+    def generate site
+      site.pages.select {|p| p.data['layout'] == 'search' }.each do |page|
+        SearchPaginator.new(page).generate(site)
+      end
+    end
+  end
+
+  class SearchPaginator
+    include PaginationGenerator
+
+    def initialize page
+      @page = page
+    end
+
+    def source_page site
+      @page
+    end
+
+    def searchterm
+      @page.data['searchterm']
+    end
+
+    def num_pages site
+      RageRender::search(site, searchterm).each_slice(RESULTS_PER_PAGE).size
+    end
+
+    def permalink
+      "/search/id/#{searchterm.hash}/:number"
+    end
+  end
+
+  def self.search site, searchterm
+    return [] unless searchterm
+    site.collections['comics'].docs.reject {|c| SPECIAL_COMIC_SLUGS.include? c.data['slug'] }.select do |comic|
+      [
+        *comic.data.fetch('tags', []),
+        comic.content,
+        *comic.data.fetch('authornotes', []).flat_map {|n| n['comment'] },
+      ].map(&:downcase).any? {|c| c.include?(searchterm.downcase) }
+    end
+  end
+
   class SearchDrop < Jekyll::Drops::Drop
     extend Pipettes
 
     private delegate_method_as :data, :fallback_data
     data_delegator 'searchterm'
+    data_delegator 'number'
+    private :number
 
     def searched
       !searchterm.nil?
@@ -19,22 +72,19 @@ module RageRender
     def_loop :searchresults, *PaginatedComicDrop.own_methods
     def searchresults
       return [] unless searched
-      @results ||= begin
-        comics = @obj.site.collections['comics'].docs.select do |comic|
-          [
-            *comic.data.fetch('tags', []),
-            comic.content,
-            *comic.data.fetch('authornotes', []).flat_map {|n| n['comment'] },
-          ].map(&:downcase).any? {|c| c.include?(searchterm.downcase) }
-        end
-        comics.map do |comic|
-          PaginatedComicDrop.new(comic, comics)
-        end
+      comics = all_results[number-1]
+      comics.map do |comic|
+        PaginatedComicDrop.new(comic, comics)
       end
     end
 
     def foundresults
       searchresults.any?
+    end
+
+    private
+    def all_results
+      @all_results ||= RageRender::search(@obj.site, searchterm).each_slice(RESULTS_PER_PAGE).to_a
     end
   end
 end
